2763bd7ba1b933331cac3e5b2565d8e72d4061b4
Eugenia Grieff <eugeniagrieff@gmail.com>
2015-12-21 15:31:55 +0000

    more solutions

--- basic/11.js
+++ basic/11.js
@@ -1,5 +1,28 @@
-// Exercise 9 - w3resource - JavaScript Basics
-// Write a JavaScript program to calculate days left until next Christmas.
+// Exercise 11 - w3resource - JavaScript Basics
+// Write a JavaScript program to convert temperatures to and from celsius, fahrenheit. Go to the editor
+
+/*[ Formula : c/5 = (f-32)/9 [ where c = temperature in celsius and f = temperature in fahrenheit ]
+Expected Output :
+    60°C is 140 °F
+45°F is 7.222222222222222°C */
+
+function cToF(celsius)
+{
+    var cTemp = celsius;
+    var cToFahr = cTemp * 9 / 5 + 32;
+    var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToC(fahrenheit)
+{
+    var fTemp = fahrenheit;
+    var fToCel = (fTemp - 32) * 5 / 9;
+    var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+}
+cToF(60);
+fToC(45);
 
 
 
--- basic/12.js
+++ basic/12.js
@@ -1,5 +1,7 @@
-// Exercise 9 - w3resource - JavaScript Basics
-// Write a JavaScript program to calculate days left until next Christmas.
+// Exercise 12 - w3resource - JavaScript Basics
+//Write a JavaScript program to get the website URL (loading page).
+
+alert(document.URL);
 
 
 
